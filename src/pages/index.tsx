aa19fe54bfc3eee79c63204ee7862553addede8c
defikintaro <defikintaro@protonmail.com>
2023-02-08 03:06:07 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,33 +52,6 @@ export default function Home() {
     <>
       <h2 className="text-3xl font-bold underline">This page is intended to filter out the tweets with addresses</h2>
       {/* <h2 className="text-3xl font-bold underline">{dataFeed}</h2> */}
-      <table className="table-auto">
-  <thead>
-    <tr>
-      <th>Song</th>
-      <th>Artist</th>
-      <th>Year</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-      <td>Malcolm Lockyer</td>
-      <td>1961</td>
-    </tr>
-    <tr>
-      <td>Witchy Woman</td>
-      <td>The Eagles</td>
-      <td>1972</td>
-    </tr>
-    <tr>
-      <td>Shining Star</td>
-      <td>Earth, Wind, and Fire</td>
-      <td>1975</td>
-    </tr>
-  </tbody>
-</table>
-
       <div className="flex flex-col">
         <div className="overflow-x-auto">
           <div className="p-1.5 w-full inline-block align-middle">
